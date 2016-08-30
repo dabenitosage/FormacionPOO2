d5ab666872ccb66af6b15fe96f17f91edd3a0447
David <david.benito@sage.com>
2016-08-30 12:54:07 +0200

    New formacion poo

---
+++ FormacionPOO/FormacionPOO/Motor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FormacionPOO
+{
+    public class Motor
+    {
+        public string Combustible { get; set; }
+        public int Cilindrada { get; set; }
+        public int Potencia { get; set; }
+    }
+}
